d49533d48113b1b6aadae0ac896cb6342fcadcec
root <lancellottimattia@protonmail.com>
2022-03-29 15:21:48 +0000

    Refining parameters handling

--- Main.ps1
+++ Main.ps1
@@ -1,3 +1,13 @@
+# TODO Validate parameters
+
+[CmdletBinding(DefaultParameterSetName = 'A')]
+Param(
+     [Parameter(Mandatory)][string] $Path,
+     [Parameter(Mandatory)][string] $Target,
+     [Parameter(ParameterSetName='A', Mandatory)][string] $Chain,
+     [Parameter(ParameterSetName='B', Mandatory)][string] $Unchain
+)
+
 function Check-Permissions {
      $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
      if ( !($currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) ) {
@@ -6,6 +16,19 @@ function Check-Permissions {
      }
 }
 
+<#
+.NAME Link-Files
+
+.Description
+This is a PowerShell module that wants to emulate GNU Stow's behaviour on Windows
+
+.PARAMETER Target
+The target directory that contains the files you want to link.
+
+.PARAMETER Path
+The directory in which all the links are going to be created.
+
+#>
 function Link-Files {
      Param(
           [Parameter(Mandatory=$true)][string] $Target,
@@ -17,9 +40,8 @@ function Link-Files {
 
      # List all files in the `Path` directroy and then linking the in the
      # `Target` directory with the same name.
+     # TODO Support for directories
      Get-ChildItem -File | ForEach-Object {
           New-Item -ItemType SymbolicLink -Path "$Path\$_" -Target $Target
      }
 }
-
-Export-ModuleMember -Function Link-Files
